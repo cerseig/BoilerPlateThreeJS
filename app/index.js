319012e3053e75fa1cb51dad5cde0b422fe8a787
GRAND Circé <cgrand@g-a101a-09.local>
2017-10-23 16:36:18 +0200

    Add kernel + cage

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import './index.css';
 
 import App from './scripts/App';
-import Sound from './scripts/Sound';
 
 window.app = new App();
--- app/scripts/App.js
+++ app/scripts/App.js
@@ -1,4 +1,8 @@
+import Sound from './Sound';
 import texture4 from '../sources/img/texture4.jpg';
+import texture3 from '../sources/img/texture3.jpg';
+import yellowclaw from '../sources/sounds/yellowclaw.mp3';
+import OrbitControls from 'three/examples/js/controls/OrbitControls';
 
 // TODO : add Dat.GUI
 // TODO : add Stats
@@ -10,7 +14,6 @@ export default class App {
 
     constructor() {
 
-      console.log(this);
           // initialiser le moteur de rendu
        this.renderer = new THREE.WebGLRenderer()
        this.renderer.setSize(width, height)
@@ -29,12 +32,16 @@ export default class App {
 
        let kernel_shape = new THREE.SphereGeometry(0.05,50, 50)
        let textureLoader = new THREE.TextureLoader();
-       let kernel_texture = textureLoader.load(texture4)
-       console.log(texture4);
-       let kernel_material = new THREE.MeshBasicMaterial({color: 0xF9CDAD, emissive: 0x000000, map: kernel_texture})
+       let kernel_texture = textureLoader.load(texture3)
+       let kernel_material = new THREE.MeshBasicMaterial({color: 0xffffff, map: kernel_texture})
        this.kernel = new THREE.Mesh( kernel_shape, kernel_material )
        // ajouter l'objet à la scène
        this.scene.add( this.kernel )
+
+       let cage_shape = new THREE.IcosahedronGeometry(0.2)
+       let cage_material = new THREE.MeshBasicMaterial({color: 0xFC9D9A, wireframe: true})
+       this.cage = new THREE.Mesh( cage_shape, cage_material )
+       this.scene.add( this.cage )
 
        // initialiser la lumière directionnelle
        this.directionalLight = new THREE.DirectionalLight(0xffffff, 1)
@@ -45,14 +52,30 @@ export default class App {
        this.renderBind = this.render.bind(this);
        this.renderBind();
 
-        this.renderer.animate( this.render.bind(this) );
+       let OrbitControls;
+       OrbitControls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
+
+       this.renderer.animate( this.render.bind(this) );
+
+       this.audio = new Sound( yellowclaw, 102, .3, () => {
+         this.audio.play()
+       }, true )
+       this.kick = this.audio.createKick(50, 0.5, 50, () => {console.log('Kick!')}, () => {console.log('No kick')} )
+       this.kick.on()
+
     }
 
     render() {
 
+      // this.audio.analyserNode.getSpectrum()
+      // this.kernel.scale.x =  1 + (frequencyDataArray[0] / 255)
+      // this.kernel.scale.y =  1 + (frequencyDataArray[0] / 255)
+
       // requestAnimationFrame( this.renderBind )
       this.kernel.rotation.x += 0.002
       this.kernel.rotation.y += 0.005
+      this.cage.rotation.x += 0.005
+      this.cage.rotation.y += 0.002
       // on fait le rendu de la scène
       this.renderer.render( this.scene, this.camera )
     }
